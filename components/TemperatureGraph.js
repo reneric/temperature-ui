67d10397893e074bff54335393f261896d720125
Ren.simmons <rsimmo4@gmail.com>
2024-06-17 18:51:39 -0500

    update polling

--- components/TemperatureGraph.js
+++ components/TemperatureGraph.js
@@ -1,3 +1,5 @@
+// components/TemperatureGraph.js
+
 "use client";
 
 import React, { useEffect, useState } from 'react';
@@ -32,7 +34,7 @@ const TemperatureGraph = () => {
   const [latestTemp, setLatestTemp] = useState(null);
   const [latestHumidity, setLatestHumidity] = useState(null);
 
-  const fetchData = () => {
+  useEffect(() => {
     axios.get('http://192.168.86.7:8000/temperature')
       .then(response => {
         const temperatureData = response.data.map(d => ({
@@ -48,14 +50,6 @@ const TemperatureGraph = () => {
         }
       })
       .catch(error => console.error('Error fetching data:', error));
-  };
-
-  useEffect(() => {
-    fetchData(); // Fetch data on component mount
-
-    const intervalId = setInterval(fetchData, 60000); // Fetch data every 60 seconds
-
-    return () => clearInterval(intervalId); // Clear interval on component unmount
   }, []);
 
   const chartData = {
@@ -68,9 +62,10 @@ const TemperatureGraph = () => {
         backgroundColor: 'rgba(75, 192, 192, 0.2)',
         fill: false,
         tension: 0.1,
-        borderWidth: 0.5,
+        borderWidth: 1,
         pointRadius: 0,
         hitRadius: 0,
+        pointRadius: 0,
         pointHoverRadius: 0,
       },
       {
@@ -80,14 +75,14 @@ const TemperatureGraph = () => {
         backgroundColor: 'rgba(153, 102, 255, 0.2)',
         fill: false,
         tension: 0.1,
-        borderWidth: 0.5,
+        borderWidth: 1,
         pointRadius: 0,
         hitRadius: 0,
+        pointRadius: 0,
         pointHoverRadius: 0,
       },
     ],
   };
-
   const options = {
     responsive: true,
     maintainAspectRatio: false,
